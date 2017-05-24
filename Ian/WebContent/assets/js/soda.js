080f74beab3d06e6d59898237992bd3989717fc9
kjc3590 <kjc3590@naver.com>
2017-05-24 10:30:30 +0900

    start

---
+++ Ian/WebContent/assets/js/soda.js
@@ -0,0 +1,9 @@
+jQuery(window).on('load', function(){
+	if ($(window).width() > 768){
+		$('.carousel-inner>.a0').attr('src','2.jpg');
+		$('.carousel-inner>.a1').attr('src','2.jpg');
+		$('.carousel-inner>.a2').attr('src','2.jpg');
+		$('.carousel-inner>.a3').attr('src','2.jpg');
+		$('.carousel-inner>.a4').attr('src','2.jpg');
+	}
+});
